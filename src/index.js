693a31ee3ffebe8fa3d8c7f0c71b3102062164db
guzhongren <guzhongren@live.cn>
2018-01-24 18:18:48 +0800

    optimizate index.js

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ define([
          */
         constructor: function (view){
             this._view =  view;
-            this._createSiglePopup();
+            // this._createSiglePopup();
         },
         /**
          * 创建pop dom
@@ -28,13 +28,17 @@ define([
          * 显示pop
          * @description 弹出框的位置为鼠标点右下角5px
          * @param mapPoint {Point} 鼠标经过的点
-         * @param content {string} 要在弹窗框中显示的内容
+         * @param content {string| HTML} 要在弹窗框中显示的内容
          * @param style{CSS | style Object} 弹出框的样式，默认样式为"width": "300px", "z-index": "888", "position": "absolute"
          */
         show: function(mapPoint, content, style){
+            if(this._tempPopupDom) {
+                this.destroy();
+            };
+            this._createSiglePopup();
             this._content = content;
             let tempPosition = this._view.toScreen(mapPoint);
-            this._style= Object.assign(style, {"background-color":"#fff","top": tempPosition.y + 5+"px", "left":  tempPosition.x + 5+"px", "width": "300px", "z-index": "888", "position": "absolute", "word-wrap":"break-word"});
+            this._style= Object.assign(style, {"background-color":"#fff","top": tempPosition.y + 10+"px", "left":  tempPosition.x + 10+"px", "min-width": "300px", "z-index": "888", "position": "absolute", "word-wrap":"break-word"});
             domStyle.set(this._tempPopupDom,this._style);
             domConstruct.empty(this._tempPopupDom);
             domConstruct.place(this._content, this._tempPopupDom);
@@ -44,7 +48,6 @@ define([
          */
         destroy: function(){
             this._tempPopupDom && domConstruct.destroy(this._tempPopupDom);
-            this._createSiglePopup();
         }
     })
     
